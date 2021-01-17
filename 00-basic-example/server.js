f8cadc128c9e68af2e222c9b6d8cf4471fa5e6a4
Clément Ollivier <clement.o2p@gmail.com>
2021-01-17 12:16:22 +0100

    :construction: Serve basic HTML page

--- 00-basic-example/server.js
+++ 00-basic-example/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+/* In real life, static files are better served by a web server like Apache or NginX */
+app.use("/", express.static(__dirname + "/public"));
+
 app.listen("8000", () => {
   console.log("App started on port 8000");
 });
