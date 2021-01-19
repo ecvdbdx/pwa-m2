913d839be95d7ef67ecf884da470039f96c0a3f0
Clément Ollivier <clement.o2p@gmail.com>
2021-01-19 14:11:59 +0100

    :ok_hand: Use cacheFirst strategy

--- 00-basic-example/public/sw.js
+++ 00-basic-example/public/sw.js
@@ -1,15 +1,23 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("v1").then((cache) => {
-      return cache.addAll(["/index.html"]);
+      return cache.addAll(["/"]);
     })
   );
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).catch(function () {
-      return fetch(event.request);
-    })
+    caches
+      .match(event.request)
+      .then((responseFromCache) => {
+        if (responseFromCache) {
+          return responseFromCache;
+        }
+        return fetch(event.request);
+      })
+      .catch(() => {
+        return fetch(event.request);
+      })
   );
 });
