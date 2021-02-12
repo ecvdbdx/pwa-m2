993a7498c871c613da94bc798f007e5467e30933
Clément Ollivier <clement.o2p@gmail.com>
2021-02-12 10:34:10 +0100

    :sparkles: 12-custom-response

---
+++ 12-custom-response/public/main.js
@@ -0,0 +1,62 @@
+import { Workbox } from "workbox-window";
+
+if ("serviceWorker" in navigator) {
+  const sw = new Workbox("/sw.js");
+
+  window.addEventListener("load", () => {
+    if ("Notification" in window) {
+      Notification.requestPermission();
+    }
+
+    sw.register();
+
+    sw.addEventListener("waiting", () => {
+      const userAgreesToUpdate = window.confirm(
+        "Une nouvelle version est disponible, cliquez ci-dessous pour profiter des mises à jour"
+      );
+      /* When a new service worker is found (meaning new content as the hashes of static assets will be different),
+         ask the user to opt in for the updates, and reload the page */
+      if (userAgreesToUpdate) {
+        sw.addEventListener("controlling", () => {
+          window.location.reload();
+        });
+      }
+      sw.messageSW({ type: "SKIP_WAITING" });
+    });
+  });
+}
+
+/* Post form */
+
+const form = document.forms[0];
+
+const responseWrapper = document.getElementById("responseWrapper");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const formData = new FormData(this);
+
+  const body = [];
+
+  for (const formElement of formData) {
+    const key = formElement[0];
+    const value = formElement[1];
+
+    body.push({ [key]: value });
+  }
+
+  fetch("/answers", {
+    headers: { "Content-type": "application/json; charset=UTF-8" },
+    method: "POST",
+    body: JSON.stringify(body),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((jsonResponse) => {
+      responseWrapper.innerText = jsonResponse.data;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
---
+++ 12-custom-response/server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* In real life, static files are better served by a web server like Apache or NginX */
+app.use("/", express.static(__dirname + "/dist"));
+
+app.post("/answers", (req, res, next) => {
+  console.log(req.body);
+  res.send({
+    data: "Thank you for answering !",
+  });
+});
+
+app.listen("8012", () => {
+  console.log("App started on port 8012");
+});
